be34b65564d650c5479123a34251b2ab000a9d40
Michael Kinder <michael@hostbend.net>
2021-02-02 17:33:38 -0500

    chore: hide language picker if javascript is disabled

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -3,6 +3,12 @@ import React from 'react';
 const Layout = ({ children }) => {
   return (
     <div className="relative bg-gray-50 dark:bg-gray-900 overflow-hidden">
+      <noscript>
+        <style>{`
+          #languagePicker {display:none;}
+        `}
+        </style>
+      </noscript>
       <div>{children}</div>
     </div>
   );
