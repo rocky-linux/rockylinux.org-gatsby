a6205cab0a322d9db63f3e6654008447ecb9b66b
Michael Kinder <mkinder@rockylinux.org>
2021-02-14 22:43:31 -0500

    chore: add rss support

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,6 +57,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-postcss`,
+    `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
