6bc24150efb6a652d35bac0992a31c7b71c4b598
ressonix <michael@hostbend.net>
2021-03-02 00:48:43 -0500

    fix: testing if rss is the issue with localization rendering

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-postcss`,
-    `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
