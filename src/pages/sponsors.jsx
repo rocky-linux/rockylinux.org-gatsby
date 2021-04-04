de3fe7d24838bf0e2774fa8a05afec1549bcb0b9
Michael Kinder <60057409+ressonix@users.noreply.github.com>
2021-04-03 20:53:31 -0400

    chore: update column count and alt image

--- src/pages/sponsors.jsx
+++ src/pages/sponsors.jsx
@@ -47,7 +47,7 @@ const Sponsors = ({ data, pageContext: { locale: language } }) => {
             </div>
           </div>
           <div className="px-4 pb-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-            <div className="grid gap-5 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
+            <div className="grid gap-5 lg:grid-cols-2 sm:max-w-sm sm:mx-auto lg:max-w-full">
               <div className="overflow-hidden transition-shadow duration-300 rounded">
                 <a href="https://ctrliq.com/">
                   <img
