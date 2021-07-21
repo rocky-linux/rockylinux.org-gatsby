eacd00ec04886f5fc0307fcb5a2e2c464e76598e
Michael Kinder <michael@hostbend.net>
2021-07-21 15:18:09 -0400

    chore: added dotslash studio to partners

--- src/pages/partners.jsx
+++ src/pages/partners.jsx
@@ -6,12 +6,13 @@ import SEO from '../components/seo';
 import Header from '../components/header';
 import Footer from '../components/footer';
 
-import CIQLogo from '../images/ciq-alt.png';
+import CIQLogo from '../images/ciq.png';
 import FossHostLogo from '../images/fosshost.png';
 import MattermostLogo from '../images/mattermost.png';
 import NaverLogo from '../images/naver.png';
 import FastlyLogo from '../images/fastly.png';
 import ArmLogo from '../images/arm.png';
+import DotSlashStudioLogo from '../images/dotslash.png';
 
 const Partners = ({ data, pageContext: { locale: language } }) => {
   const translate = Translate('partners');
@@ -51,7 +52,7 @@ const Partners = ({ data, pageContext: { locale: language } }) => {
             </div>
           </div>
           <div className="rounded-lg bg-gray-200 dark:bg-gray-900 overflow-hidden shadow divide-y divide-gray-200 dark:divide-gray-800 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px">
-            <div className="col-span-2 rounded-tl-lg rounded-tr-lg sm:rounded-tr-none relative group bg-gray-500 dark:bg-gray-800 p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-green-500">
+            <div className="rounded-tl-lg rounded-tr-lg sm:rounded-tr-none relative group bg-gray-500 dark:bg-gray-800 p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-green-500">
               <div>
                 <span className="rounded-lg inline-flex p-3">
                   <img
@@ -98,6 +99,31 @@ const Partners = ({ data, pageContext: { locale: language } }) => {
                 </h3>
                 <p className="mt-2 text-sm text-gray-200">
                   {translate('partners.4.description')}
+                </p>
+              </div>
+            </div>
+            <div className="relative group bg-gray-500 dark:bg-gray-800 p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-green-500">
+              <div>
+                <span className="rounded-lg inline-flex p-3">
+                  <img
+                      className="object-cover w-full"
+                      src={DotSlashStudioLogo}
+                      alt="DotSlash Studio"
+                  />
+                </span>
+              </div>
+              <div className="mt-8">
+                <h3 className="text-lg font-medium text-gray-50">
+                  <a href="https://www.dotslash.studio/" className="focus:outline-none">
+                    <span className="absolute inset-0" aria-hidden="true" />
+                    {translate('partners.5.name')}
+                    <span className="ml-2 bg-green-700 text-green-100 py-1 px-2 text-xs rounded-full self-center">
+                      {translate('partners.5.grade')}
+                    </span>
+                  </a>
+                </h3>
+                <p className="mt-2 text-sm text-gray-200">
+                  {translate('partners.5.description')}
                 </p>
               </div>
             </div>
