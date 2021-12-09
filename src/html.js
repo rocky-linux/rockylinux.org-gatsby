b157030dbd944831596517bacb888db539af318a
Michael Kinder <michael@dotslash.studio>
2021-12-09 12:15:13 -0500

    chore: run pre-commit

--- src/html.js
+++ src/html.js
@@ -1,5 +1,5 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function HTML(props) {
   return (
@@ -12,7 +12,12 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
-        <script src="https://cdn.usefathom.com/script.js" data-spa="auto" data-site="QDEPAHYS" defer></script>
+        <script
+          src="https://cdn.usefathom.com/script.js"
+          data-spa="auto"
+          data-site="QDEPAHYS"
+          defer
+        ></script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
@@ -24,7 +29,7 @@ export default function HTML(props) {
         {props.postBodyComponents}
       </body>
     </html>
-  )
+  );
 }
 
 HTML.propTypes = {
@@ -34,4 +39,4 @@ HTML.propTypes = {
   preBodyComponents: PropTypes.array,
   body: PropTypes.string,
   postBodyComponents: PropTypes.array,
-}
+};
