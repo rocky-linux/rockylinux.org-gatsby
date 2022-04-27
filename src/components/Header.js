d67b3eea4b58f3052b31f69c4b87bda1b72abecb
Thijs van der Woude <thijsvanderwoude99@gmail.com>
2022-04-27 14:25:22 +0200

    Finalize Header.js darkmode

--- src/components/Header.js
+++ src/components/Header.js
@@ -308,7 +308,7 @@ const Header = ({ pageContext: { locale: language } }) => {
             focus
             className="z-20 absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
           >
-            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50 dark:divide-gray-700">
               <div className="pt-5 pb-6 px-5 dark:bg-gray-800">
                 <div className="flex items-center justify-between">
                   <div>
