5cd61ebcd0f01e1f8c78f15561349bd6bc37fe0a
Gabriel Graves <64073162+NebraskaCoder@users.noreply.github.com>
2022-08-13 21:11:58 +0000

    fix: changed colors on 404 page for dark mode The text/image colors on the 404 page changed to white to match theme

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,15 +4,19 @@ import Layout from '../components/Layout';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Shrug from '../images/shrug.svg';
+import ShrugDark from '../images/shrug-dark.svg';
 
 const NotFoundPage = () => {
   return (
     <Layout>
       <SEO title="404" />
       <Header pageContext="{locale: language}" />
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-center mt-20">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-gray-400 dark:text-gray-200">
+        <div className="flex dark:hidden items-center justify-center mt-20">
           <img src={Shrug} className="h-20" />
+        </div>
+        <div className="hidden dark:flex items-center justify-center mt-20">
+          <img src={ShrugDark} className="h-20" />
         </div>
         <div className="flex items-center justify-center mt-10">
           <h1 className="text-5xl font-bold">We couldn't find that page.</h1>
