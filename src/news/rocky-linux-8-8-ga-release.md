e8dcbc26bc456ffe159f99e4dc5c452aea6a5342
Brian Clemens <brian@resf.org>
2023-05-20 01:26:18 +0900

    8.8 torrents

--- src/news/rocky-linux-8-8-ga-release.md
+++ src/news/rocky-linux-8-8-ga-release.md
@@ -53,6 +53,7 @@ We would like to thank the following individuals in particular for their contrib
 * Brian Clemens
 * Chris Stackpole
 * David H
+* Ezequiel Bruni
 * Louis Abel
 * Lukas Magauer
 * Mustafa Gezen
--- src/pages/download.js
+++ src/pages/download.js
@@ -415,7 +415,7 @@ const DownloadPage = ({ data, pageContext: { locale: language } }) => {
                           |&nbsp;
                           <a
                             className="font-medium text-green-600 dark:text-green-500"
-                            href="https://download.rockylinux.org/pub/rocky/8/isos/x86_64/Rocky-8.7-x86_64-dvd1.torrent"
+                            href="https://download.rockylinux.org/pub/rocky/8/isos/x86_64/Rocky-8.8-x86_64-dvd1.torrent"
                             onClick={() => downloadEvent('RL8: Torrent x86_64')}
                           >
                             {translate('body.torrent')}
@@ -470,7 +470,7 @@ const DownloadPage = ({ data, pageContext: { locale: language } }) => {
                           |&nbsp;
                           <a
                             className="font-medium text-green-600 dark:text-green-500"
-                            href="https://download.rockylinux.org/pub/rocky/8/isos/aarch64/Rocky-8.7-aarch64-dvd1.torrent"
+                            href="https://download.rockylinux.org/pub/rocky/8/isos/aarch64/Rocky-8.8-aarch64-dvd1.torrent"
                             onClick={() =>
                               downloadEvent('RL8: Torrent aarch64')
                             }
