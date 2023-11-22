8954a6fbb0d12acbd78c57446a4e01125b126f87
Louis Abel <tucklesepk@gmail.com>
2023-11-22 14:20:09 -0700

    release 8.9

--- src/pages/download.js
+++ src/pages/download.js
@@ -391,7 +391,7 @@ const DownloadPage = ({ data, pageContext: { locale: language } }) => {
                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                           <a
                             className="font-medium text-green-600 dark:text-green-500"
-                            href="https://download.rockylinux.org/pub/rocky/8/isos/x86_64/Rocky-8.8-x86_64-minimal.iso"
+                            href="https://download.rockylinux.org/pub/rocky/8/isos/x86_64/Rocky-8.9-x86_64-minimal.iso"
                             onClick={() => downloadEvent('RL8: Minimal x86_64')}
                           >
                             {translate('body.minimal')}
@@ -399,7 +399,7 @@ const DownloadPage = ({ data, pageContext: { locale: language } }) => {
                           |&nbsp;
                           <a
                             className="font-medium text-green-600 dark:text-green-500"
-                            href="https://download.rockylinux.org/pub/rocky/8/isos/x86_64/Rocky-8.8-x86_64-dvd1.iso"
+                            href="https://download.rockylinux.org/pub/rocky/8/isos/x86_64/Rocky-8.9-x86_64-dvd1.iso"
                             onClick={() => downloadEvent('RL8: DVD x86_64')}
                           >
                             {translate('body.dvd')}
@@ -407,7 +407,7 @@ const DownloadPage = ({ data, pageContext: { locale: language } }) => {
                           |&nbsp;
                           <a
                             className="font-medium text-green-600 dark:text-green-500"
-                            href="https://download.rockylinux.org/pub/rocky/8/isos/x86_64/Rocky-8.8-x86_64-boot.iso"
+                            href="https://download.rockylinux.org/pub/rocky/8/isos/x86_64/Rocky-8.9-x86_64-boot.iso"
                             onClick={() => downloadEvent('RL8: Boot x86_64')}
                           >
                             {translate('body.boot')}
@@ -415,7 +415,7 @@ const DownloadPage = ({ data, pageContext: { locale: language } }) => {
                           |&nbsp;
                           <a
                             className="font-medium text-green-600 dark:text-green-500"
-                            href="https://download.rockylinux.org/pub/rocky/8/isos/x86_64/Rocky-8.8-x86_64-dvd1.torrent"
+                            href="https://download.rockylinux.org/pub/rocky/8/isos/x86_64/Rocky-8.9-x86_64-dvd1.torrent"
                             onClick={() => downloadEvent('RL8: Torrent x86_64')}
                           >
                             {translate('body.torrent')}
@@ -444,7 +444,7 @@ const DownloadPage = ({ data, pageContext: { locale: language } }) => {
                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                           <a
                             className="font-medium text-green-600 dark:text-green-500"
-                            href="https://download.rockylinux.org/pub/rocky/8/isos/aarch64/Rocky-8.8-aarch64-minimal.iso"
+                            href="https://download.rockylinux.org/pub/rocky/8/isos/aarch64/Rocky-8.9-aarch64-minimal.iso"
                             onClick={() =>
                               downloadEvent('RL8: Minimal aarch64')
                             }
@@ -454,7 +454,7 @@ const DownloadPage = ({ data, pageContext: { locale: language } }) => {
                           |&nbsp;
                           <a
                             className="font-medium text-green-600 dark:text-green-500"
-                            href="https://download.rockylinux.org/pub/rocky/8/isos/aarch64/Rocky-8.8-aarch64-dvd1.iso"
+                            href="https://download.rockylinux.org/pub/rocky/8/isos/aarch64/Rocky-8.9-aarch64-dvd1.iso"
                             onClick={() => downloadEvent('RL8: DVD aarch64')}
                           >
                             {translate('body.dvd')}
@@ -462,7 +462,7 @@ const DownloadPage = ({ data, pageContext: { locale: language } }) => {
                           |&nbsp;
                           <a
                             className="font-medium text-green-600 dark:text-green-500"
-                            href="https://download.rockylinux.org/pub/rocky/8/isos/aarch64/Rocky-8.8-aarch64-boot.iso"
+                            href="https://download.rockylinux.org/pub/rocky/8/isos/aarch64/Rocky-8.9-aarch64-boot.iso"
                             onClick={() => downloadEvent('RL8: Boot aarch64')}
                           >
                             {translate('body.boot')}
@@ -470,7 +470,7 @@ const DownloadPage = ({ data, pageContext: { locale: language } }) => {
                           |&nbsp;
                           <a
                             className="font-medium text-green-600 dark:text-green-500"
-                            href="https://download.rockylinux.org/pub/rocky/8/isos/aarch64/Rocky-8.8-aarch64-dvd1.torrent"
+                            href="https://download.rockylinux.org/pub/rocky/8/isos/aarch64/Rocky-8.9-aarch64-dvd1.torrent"
                             onClick={() =>
                               downloadEvent('RL8: Torrent aarch64')
                             }
